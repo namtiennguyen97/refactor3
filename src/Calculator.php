293d485650520cdfda08406b0aeaaa66093bbd06
namnguyen <toilanam97@gmail.com>
2019-09-12 09:51:58 +0700

    DEmo Refactor

--- src/Calculator.php
+++ src/Calculator.php
@@ -1,24 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: phanluan
- * Date: 25/10/2018
- * Time: 00:02
- */
 
 class Calculator
 {
-    public function calculate($a, $b, $o) {
-        switch ($o) {
-            case '+':
-                return $a + $b;
-            case '-':
-                return $a - $b;
-            case '*':
-                return $a * $b;
-            case '/':
-                if ($b != 0) {
-                    return $a / $b;
+    const ADDITION = '+';
+    const SUBTRACTION = '-';
+    const MULTIPLICATION = '*';
+    const DIVISION = '/';
+
+    public function calculate($firstOperand, $secondOperand, $operator) {
+        switch ($operator) {
+            case self::ADDITION:
+                return $firstOperand + $secondOperand;
+            case self::SUBTRACTION:
+                return $firstOperand - $secondOperand;
+            case self::MULTIPLICATION:
+                return $firstOperand * $secondOperand;
+            case self::DIVISION:
+                if ($secondOperand != 0) {
+                    return $firstOperand / $secondOperand;
                 } else {
                     echo("Can not divide by 0");
                     break;
